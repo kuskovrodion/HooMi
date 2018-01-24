de0390965026ade5875a2f7590dbd16f84d1d098
kuskovrodion <rodion.kuskov@gmail.com>
2018-01-24 15:54:50 +0200

    small bugs were fixed

--- HooMi/Controllers/AddNewMixViewContoller.swift
+++ HooMi/Controllers/AddNewMixViewContoller.swift
@@ -17,6 +17,12 @@ class AddNewMixViewContoller: UIViewController, UITextFieldDelegate, UITextViewD
         case vo = "Vortex"
     }
     
+    var imagePicker: UIImagePickerController!
+    var takenImage: UIImage!
+    
+    var test = 0
+    var deleteClicks = 0
+    
     @IBAction func takingImageBarButtom(_ sender: UIBarButtonItem) {
         takePhotoToAddingMix()
     }
@@ -44,6 +50,71 @@ class AddNewMixViewContoller: UIViewController, UITextFieldDelegate, UITextViewD
     }
     
     @IBOutlet weak var bowlNameButton: UIButton!
+    @IBAction func shareMixButton(_ sender: UIButton) {
+        checkAndSaving()
+    }
+    
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet weak var AddNewMixName: UITextField!
+    @IBOutlet weak var AddNewMixDescription: UITextView!
+    
+    @IBOutlet weak var tabackNameFirstField: UITextField!
+    @IBOutlet weak var tabackNameSecondField: UITextField!
+    @IBOutlet weak var tabackNameThirdField: UITextField!
+    @IBOutlet weak var tabackNameForthField: UITextField!
+    
+    @IBOutlet weak var tabackPercentsFirstField: UITextField!
+    @IBOutlet weak var tabackPercentsSecondField: UITextField!
+    @IBOutlet weak var tabackPercentsThirdField: UITextField!
+    @IBOutlet weak var tabackPercentsForthField: UITextField!
+    
+    @IBAction func addRowButton(_ sender: UIButton) {
+            test += 1
+            if test == 1 {
+                tabackNameThirdField.isHidden = false
+                tabackPercentsThirdField.isHidden = false
+                outletDeleteRowButton.isHidden = false
+            } else if test == 2 {
+                tabackNameForthField.isHidden = false
+                tabackPercentsForthField.isHidden = false
+                outletDeleteRowButton.isHidden = false
+                test = 0
+            }
+    }
+    
+    @IBOutlet weak var outletDeleteRowButton: UIButton!
+    
+    @IBAction func deleteRowButton(_ sender: UIButton) {
+        deleteClicks += 1
+        if deleteClicks == 1 {
+            if tabackNameForthField.isHidden == true {
+                tabackNameThirdField.isHidden = true
+                tabackPercentsThirdField.isHidden = true
+                outletDeleteRowButton.isHidden = true
+                deleteClicks = 0
+            } else {
+                tabackNameForthField.isHidden = true
+                tabackPercentsForthField.isHidden = true
+//                outletDeleteRowButton.isHidden = true
+            }
+        } else if deleteClicks == 2 {
+            tabackNameThirdField.isHidden = true
+            tabackPercentsThirdField.isHidden = true
+            outletDeleteRowButton.isHidden = true
+            deleteClicks = 0
+        }
+    }
+    
+    @IBOutlet weak var mixStrengthLabel: UILabel!
+    @IBAction func mixStrengthSlider(_ sender: UISlider) {
+        mixStrengthLabel.text = String(Int(sender.value))
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
     
     func hideAndAppear() {
         bowlButton.forEach { (button) in
@@ -54,10 +125,7 @@ class AddNewMixViewContoller: UIViewController, UITextFieldDelegate, UITextViewD
         }
     }
     
-
-    @IBAction func shareMixButton(_ sender: UIButton) {
-        
-        
+    func checkAndSaving() {
         if takenImage == nil {
             let ErrorAlert = UIAlertController(title: "Oops..", message: "Choose image", preferredStyle: .alert)
             
@@ -65,9 +133,13 @@ class AddNewMixViewContoller: UIViewController, UITextFieldDelegate, UITextViewD
             ErrorAlert.addAction(ErrorAction)
             
             self.present(ErrorAlert, animated: true, completion: nil)
+        } else if  tabackNameFirstField.text == "" && tabackPercentsFirstField.text == "" && tabackNameSecondField.text == "" && tabackPercentsSecondField.text == "" {
+            let ErrorAlert = UIAlertController(title: "Oops..", message: "Enter minimum 2 tobaccos name and 2 tobaccos ratio", preferredStyle: .alert)
+            let ErrorAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+            ErrorAlert.addAction(ErrorAction)
+            self.present(ErrorAlert, animated: true, completion: nil)
         }
-        
-        if AddNewMixName.text != "" && AddNewMixDescription.text != "" {
+        if AddNewMixName.text != "" && AddNewMixDescription.text != "" && takenImage != nil {
             
             if tabackPercentsThirdField.isHidden == true {
                 tabackPercentsThirdField.text = "0"
@@ -83,21 +155,20 @@ class AddNewMixViewContoller: UIViewController, UITextFieldDelegate, UITextViewD
                 let mix = Mix(image: takenImage, MixName: AddNewMixName.text!, MixDesc: AddNewMixDescription.text, mixBowl: (bowlNameButton.titleLabel?.text)!, mixStrength: mixStrengthLabel.text!, firstTabackName: tabackNameFirstField.text!, firstTabackPercents: tabackPercentsFirstField.text!, secondTabackName: tabackNameSecondField.text!, secondTabackPercents: tabackPercentsSecondField.text!, thirdTabackName: tabackNameThirdField.text!, thirdTabackPercents: tabackPercentsThirdField.text!, forthTabackNamme: tabackNameForthField.text!, forthTabackPercents: tabackPercentsForthField.text!)
                 mix.saveIntoDatabase()
             }
-
+            
             self.dismiss(animated: true, completion: nil)
             
             let alert = UIAlertController(title: "Congrats!", message: "Your mix was successfully saved", preferredStyle: .alert)
             
             let action = UIAlertAction(title: "Cool!", style: .default) { (action) -> Void in
                 let present = self.storyboard?.instantiateInitialViewController()
-
+                
                 self.present(present!, animated: true, completion: nil)
             }
             
             alert.addAction(action)
             
             self.present(alert, animated: true, completion: nil)
-            
             
         } else {
             
@@ -108,35 +179,22 @@ class AddNewMixViewContoller: UIViewController, UITextFieldDelegate, UITextViewD
             
             self.present(ErrorAlert, animated: true, completion: nil)
         }
+        
     }
     
-    @IBOutlet weak var backgroundImageView: UIImageView!
-    @IBOutlet weak var AddNewMixName: UITextField!
-    @IBOutlet weak var AddNewMixDescription: UITextView!
-    
-     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         let currentText = textField.text ?? ""
         guard let stringRange = Range(range, in: currentText) else { return false }
         let updatedText = currentText.replacingCharacters(in: stringRange, with: string)
         return updatedText.count <= 80
     }
-     func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
         let currentText = textView.text ?? ""
         guard let stringRange = Range(range, in: currentText) else { return false }
         let updatedText = currentText.replacingCharacters(in: stringRange, with: text)
         return updatedText.count <= 150
     }
     
-
-    
-    var imagePicker: UIImagePickerController!
-    var takenImage: UIImage!
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-    }
-
     func takePhotoToAddingMix() {
         imagePicker = UIImagePickerController()
         imagePicker.delegate = self
@@ -152,58 +210,7 @@ class AddNewMixViewContoller: UIViewController, UITextFieldDelegate, UITextViewD
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    
-    var test = 0
-    var deleteClicks = 0
-    
-    @IBOutlet weak var tabackNameFirstField: UITextField!
-    @IBOutlet weak var tabackNameSecondField: UITextField!
-    @IBOutlet weak var tabackNameThirdField: UITextField!
-    @IBOutlet weak var tabackNameForthField: UITextField!
-    
-    @IBOutlet weak var tabackPercentsFirstField: UITextField!
-    @IBOutlet weak var tabackPercentsSecondField: UITextField!
-    @IBOutlet weak var tabackPercentsThirdField: UITextField!
-    @IBOutlet weak var tabackPercentsForthField: UITextField!
-    
-    @IBAction func addRowButton(_ sender: UIButton) {
-            test += 1
-            print("ПЛЮСИК \(test)")
-            if test == 1 {
-                tabackNameThirdField.isHidden = false
-                tabackPercentsThirdField.isHidden = false
-                outletDeleteRowButton.isHidden = false
-            } else if test == 2 {
-                tabackNameForthField.isHidden = false
-                tabackPercentsForthField.isHidden = false
-                outletDeleteRowButton.isHidden = false
-                test = 0
-            }
-    }
-    
-    @IBOutlet weak var outletDeleteRowButton: UIButton!
-    @IBAction func deleteRowButton(_ sender: UIButton) {
-        
-        deleteClicks += 1
-        print("МИНУСИК \(deleteClicks)")
-        if deleteClicks == 1 {
-            tabackNameThirdField.isHidden = true
-            tabackPercentsThirdField.isHidden = true
-        } else if deleteClicks == 2 {
-            tabackNameForthField.isHidden = true
-            tabackPercentsForthField.isHidden = true
-            outletDeleteRowButton.isHidden = true
-            deleteClicks = 0
-        }
 
-    }
-    
-    @IBOutlet weak var mixStrengthLabel: UILabel!
-    @IBAction func mixStrengthSlider(_ sender: UISlider) {
-        mixStrengthLabel.text = String(Int(sender.value))
-    }
-    
-    
 }
 
 extension AddNewMixViewContoller : UIImagePickerControllerDelegate, UINavigationControllerDelegate {
